127080d9dd7e22e941a25e166763d8059a71458d
paudieMola <44343590+paudieMola@users.noreply.github.com>
2021-10-04 16:08:24 +0100

    Down to listing 10 in Lab 2

---
+++ app/matches.py
@@ -0,0 +1,25 @@
+import pattern.object_factory as object_factory
+import app.gameimpl.x01_match as x01_match
+import app.gameimpl.t01_match as t01_match
+import domain.darts_match as darts_match
+from datatype.enums import DartMultiplier
+from domain import visit
+
+factory = object_factory.ObjectFactory()
+factory.register_builder('X01', x01_match.X01MatchBuilder())
+factory.register_builder('301', t01_match.T01MatchBuilder())
+
+x01 = factory.create('X01')
+t01 = factory.create('T01')
+match = darts_match.DartsMatch()
+
+player1_index = match.register_player('Alice')
+player2_index = match.register_player('Kalifa')
+x01.set_match(match)
+
+my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.TREBLE, 20),
+                        (DartMultiplier.SINGLE, 5)])
+result, response = x01.process_visit(player1_index, my_visit)
+print(response)
+
+my_visit = visit.Visit
